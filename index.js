5cb68e9386ff5588fe5418ae28cf235443ef2fb1
ime <imemang@gmail.com>
2020-05-16 12:29:59 +0900

    비밀 정보보호

--- index.js
+++ index.js
@@ -5,13 +5,15 @@ const port = 3000
 const bodyParser = require("body-parser");
 const { User } = require("./models/User");
 
+const config = require('./config/key');
+
 // application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: true}));
 // application/json
 app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://ime:dkssud@boilerplate-k9x99.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('mongoDB Connected...'))
   .catch(err => console.log('err'))
